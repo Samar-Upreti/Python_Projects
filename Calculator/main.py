a2a8cf2909ed761376aeff8556f736130b0d9367
Samar upreti <samarupreti252611@gmail.com>
2026-07-08 16:00:15 +0530

    added calculator - need logics

---
+++ Calculator/main.py
@@ -0,0 +1,37 @@
+import maths_logics as logic
+
+def options():
+    '''Shows Options To The User For Terminal Run'''
+    while True:
+        print("1. Square")
+        print("2. Check Even")
+        print("3. Check Odd")
+        print("4. Circumference of Circle")
+        print("5. Area of Sphere")
+        try:
+            user_select = int(input("Choose :"))
+
+            if user_select == 1:
+                a = int(input("Enter A Number :"))
+                print(logic.sq(a))
+
+            elif user_select == 2:
+                a = int(input("Enter Number To Check Even Or Not :"))
+                print(logic.is_even(a))
+
+            elif user_select == 3:
+                a = int(input("Enter Number To Check Even Or Not :"))
+                print(logic.is_odd(a))
+
+            elif user_select == 4:
+                a = int(input("Enter Radius :"))
+                print(logic.circumference_circle(a))
+                
+            elif user_select == 5:
+                a = int(input("Enter Radius :"))
+                print(logic.area_sphere(a))
+            break
+        except ValueError:
+            print("Enter Numbers only")
+            break
+options()
---
+++ Calculator/maths_logics.py
@@ -0,0 +1,15 @@
+def sq(x):
+    return x**2
+
+def is_even(x):
+    return x%2 == 0
+
+def is_odd(x):
+    return x%2 != 0
+
+def circumference_circle(x):
+    return 2*(22/7)*x
+
+def area_sphere(x):
+    solution = 4*(22/7)*(x**2)
+    return solution
